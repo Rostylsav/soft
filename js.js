b3f28c3f14c777345369efa6affc6262f6beaea8
Rostyslav <pas.ros.bor@gmail.com>
2014-01-17 14:47:23 +0200

    доробив активації checkbox-ів.

--- js.js
+++ js.js
@@ -1,21 +1,80 @@
-var i=1;
+var valueId=0;
+
+var arrayDiv= new Array();
 function getTask()
 {
-
 	var newCheckbox = document.createElement('input');
 	
-		newCheckbox.id = 'check';
+		newCheckbox.id = 'check'+valueId;
+		newCheckbox.className = 'check';
 		newCheckbox.type='checkbox';
-		newCheckbox.name='checkbox'+i;	
+		
 
     var newDiv = document.createElement('div');
 	
-		newDiv.id = 'get_task';
+		newDiv.id = 'get_task'+valueId;
+		newDiv.className = 'get_task';
+		
+	arrayDiv.push(document.getElementById('task').value);
 
 	newDiv.appendChild(document.createTextNode(document.getElementById('task').value));
+	
 	document.getElementById('task').value='';
+	
 	document.body.appendChild(newCheckbox);
-	document.body.appendChild(newDiv);
-	i++;
+	document.body.appendChild(newDiv);	
+	
+	document.getElementById('check'+valueId).addEventListener("click", isChecked,false);
+
+	valueId++;
+}
+
+function isChecked()
+{	
+	for(var i=0; i<arrayDiv.push(); i++ )
+		{
+			if(document.getElementById('check'+i).checked)
+			{
+				document.getElementById('get_task'+i).style.textDecoration ='line-through';
+				document.getElementById('get_task'+i).style.color ='#999';
+				
+			}
+			else
+			{
+				document.getElementById('check'+i).style.color ='#999';
+				document.getElementById('get_task'+i).style.textDecoration ='none';
+				document.getElementById('get_task'+i).style.color ='#111';
+			}
+		}
+}
+
+function isCheckedAll()
+{	
+	if(document.getElementById('check_all').checked)
+	{
+		for(var i=0; i<arrayDiv.push(); i++ )
+		{
+				document.getElementById('check'+i).style.color ='#ff0000';
+				document.getElementById('get_task'+i).style.textDecoration ='line-through';
+				document.getElementById('get_task'+i).style.color ='#999';
+		}
+	}
+	else
+	{
+		for(var i=0; i<arrayDiv.push(); i++ )
+		{
+			if(document.getElementById('check'+i).checked)
+			{
+				document.getElementById('get_task'+i).style.textDecoration ='line-through';
+				document.getElementById('get_task'+i).style.color ='#999';
+			}
+			else
+			{
+				document.getElementById('check'+i).style.color ='#999';
+				document.getElementById('get_task'+i).style.textDecoration ='none';
+				document.getElementById('get_task'+i).style.color ='#111';
+			}
+		}
+	}
 }
 
