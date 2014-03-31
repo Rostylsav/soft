2bcab14c49b3990fbb73b3133aeec763203007b0
Rostyk <pas.ros.bor@gmail.com>
2014-03-31 13:49:51 +0300

    1.0

--- XMLHttpRequest/script.js
+++ XMLHttpRequest/script.js
@@ -1,13 +1,36 @@
-function vote() {
-  var xhr = new XMLHttpRequest();
+(function()
+{   
+    var value=[];
+    function getData(url,callback,method) 
+    {
+        
+        var xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'test.htm', true); 
+        xhr.open(method, url, true); 
 
-  xhr.onreadystatechange = function() {  
-    if (xhr.readyState != 4) return; 
+        xhr.onreadystatechange = function() {  
+            if (xhr.readyState != 4) return; 
+            value = callback(xhr.responseText); 
+            
+        }
+        xhr.send(null); 
+        console.log(value);
+    }
 
-    alert(xhr.responseText); 
-  }
+    function getCollection(text)
+    {
+        var array_of_strings=new Array();
+        var collection=new Array();
+        array_of_strings=text.split('\n');
+        function transformToObject(item)
+        {
+            var array=item.split(',');
+            return {name:array[0],age:array[1],town:array[2]};
+        }
+        collection=array_of_strings.map(transformToObject);
+        return collection;
+    }
+    getData('test.txt', getCollection, 'GET');
 
-  xhr.send(null); 
-}
+
+})()
