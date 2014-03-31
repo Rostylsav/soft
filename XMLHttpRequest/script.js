cb942017af84a5e956ee7d197cfdc8917adeb748
Rostyk <pas.ros.bor@gmail.com>
2014-03-31 14:41:43 +0300

    1.1

--- XMLHttpRequest/script.js
+++ XMLHttpRequest/script.js
@@ -1,36 +1,30 @@
 (function()
 {   
-    var value=[];
+    var collection = [];
     function getData(url,callback,method) 
     {
-        
         var xhr = new XMLHttpRequest();
-
         xhr.open(method, url, true); 
-
         xhr.onreadystatechange = function() {  
             if (xhr.readyState != 4) return; 
-            value = callback(xhr.responseText); 
-            
+                callback(xhr.responseText);  
         }
         xhr.send(null); 
-        console.log(value);
+    }
+
+    function transformToObject(item)
+    {
+        var array=item.split(',');
+        return {name:array[0], age:array[1], town:array[2]};
     }
 
     function getCollection(text)
     {
-        var array_of_strings=new Array();
-        var collection=new Array();
-        array_of_strings=text.split('\n');
-        function transformToObject(item)
-        {
-            var array=item.split(',');
-            return {name:array[0],age:array[1],town:array[2]};
-        }
-        collection=array_of_strings.map(transformToObject);
-        return collection;
+        var array_of_strings = [];
+        array_of_strings = text.split('\n');
+        collection = array_of_strings.map(transformToObject);
     }
+
     getData('test.txt', getCollection, 'GET');
-
-
+    console.log(collection);
 })()
